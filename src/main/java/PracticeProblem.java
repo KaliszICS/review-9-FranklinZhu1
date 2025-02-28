6b30ea15d1bba408d9d4076b7b35e53ee8367901
franklin-zhu-ics4u1-02-25 <123124410+FranklinZhu1@users.noreply.github.com>
2025-02-28 14:14:31 +0000

    Update lettersToFive to use while loop rather than for loop

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,12 +1,14 @@
 public class PracticeProblem {
 
 	public static int lettersToFive(String sentence, char letter) {
-		int count = 0;
-		for (int i = 0; i < sentence.length(); ++i) {
+		if (sentence.length() == 0) return -1;
+		int count = 0, i = 0;
+		while (true) {
 			if (sentence.charAt(i) == letter) ++count;
 			if (count == 5) return i + 1;
+			else if (i == sentence.length() - 1) return -1;
+			++i;
 		}
-		return -1;
 	}
 
 	public static int countLetter(String sentence, char letter) {
